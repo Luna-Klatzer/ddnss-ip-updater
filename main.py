befe30e2f4d223098fd61293e64f4db84198d9db
Luna <dev@lklatzer.com>
2025-06-19 19:50:09 +0200

    major: Improved logging in main.py and removed deprecated HTTP calls

--- main.py
+++ main.py
@@ -5,18 +5,23 @@ import sys
 import traceback
 import requests
 
+def get_prefix():
+    return f"[{datetime.datetime.now()}]"
+
+def log(msg):
+    print(f"{get_prefix()} {msg}")
 
 def read_config():
-    with open("/usr/share/ddnss-ip-updater/config.json") as config:
+    with open("./config.json") as config:
         data = json.loads(config.read())
-        print(f"User: {data['USER']}\nPassword: {data['PASS']}\nHost: {data['HOST']}\n")
         return data['USER'], data['PASS'], data['HOST']
 
 
 class DDNSSIPChanger:
     async def wait_for_connection(self):
+        log("Awaiting connection...")
         error = True
-        url = "http://google.com"
+        url = "https://google.com"
         timeout = 5
         while error is True:
             try:
@@ -25,15 +30,19 @@ class DDNSSIPChanger:
                 await asyncio.sleep(5)
             else:
                 error = False
+        log("Connection reestablished")
 
     async def update_ip(self, user, password, host):
         # Waits until a connection is possible
+        log("Updating ip in DNS record")
         await self.wait_for_connection()
-        requests.post(f"http://ddnss.de/upd.php?user={user}&pwd={password}&host={host}")
+        requests.post(f"https://ddnss.de//upd.php?user={user}&pwd={password}&host={host}")
+        log(f"Changed ip to {new_ip}")
 
     async def run(self, user, password, host):
         # On Start it will automatically update the ip
         await self.update_ip(user, password, host)
+
         ip = requests.get('https://api.ipify.org').text
         while True:
             try:
@@ -42,10 +51,9 @@ class DDNSSIPChanger:
                 # ip is different and a request needs to be sent
                 if new_ip != ip:
                     await self.update_ip(user, password, host)
-                    print(f"Changed ip to {new_ip}")
                     ip = new_ip
 
-                await asyncio.sleep(10)
+                await asyncio.sleep(30)
 
             except (requests.ConnectionError, requests.Timeout):
                 # No connection => Waits until a connection is possible again
@@ -60,14 +68,15 @@ class DDNSSIPChanger:
 
 async def main():
     ip_changer = DDNSSIPChanger()
-    print(f"Process started at {datetime.datetime.now()}")
-
+    log("Reading config")
     user, password, host = read_config()
+    log("Starting process loop")
     while True:
         await ip_changer.run(user, password, host)
 
 
 if __name__ == '__main__':
+    log("-- Process started --")
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
     loop.run_forever()
